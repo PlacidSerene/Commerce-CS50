de2aa98437d31c3bcb4decfa718d641603bccfeb
Daniel <t.dung.lhp@gmail.com>
2021-03-10 21:57:47 -0600

    404 page added

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import authenticate, login, logout
 from django.core.validators import MinValueValidator
 from django.db import IntegrityError
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render
 from django.urls import reverse
 from django import forms
